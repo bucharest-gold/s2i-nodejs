aec2c490ef522f37a3944db8cdeefacf979b2db2
Helio Frota <00hf11@gmail.com>
2017-02-27 20:26:54 -0300

    build: fix project name and add more time sleep

--- tags.sh
+++ tags.sh
@@ -6,11 +6,11 @@ oc login -u system:admin
 for TAG in $(wget -q https://registry.hub.docker.com/v1/repositories/bucharestgold/centos7-s2i-nodejs/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}');
 do
   TAGG=${TAG//./}
-  oc new-project ${TAGG}
+  oc new-project "project${TAGG}"
   oc new-app bucharestgold/centos7-s2i-nodejs:${TAG}~https://github.com/bucharest-gold/s2i-nodejs.git
-  sleep 59
+  sleep 60
   oc expose svc/s2i-nodejs
-  sleep 20
+  sleep 30
   curl $(oc get routes | awk '{print $2}' | grep xip.io)
   printf "\n"
   echo "================================================================"
