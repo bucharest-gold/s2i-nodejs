30aca8658091e1f62f8da66c82ce8192f7c4e3a1
Helio Frota <00hf11@gmail.com>
2017-02-27 20:11:45 -0300

    build: attempt to deploy all tags

---
+++ tags.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+oc cluster up
+oc login -u system:admin
+
+for TAG in $(wget -q https://registry.hub.docker.com/v1/repositories/bucharestgold/centos7-s2i-nodejs/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}');
+do
+  TAGG=${TAG//./}
+  oc new-project ${TAGG}
+  oc new-app bucharestgold/centos7-s2i-nodejs:${TAG}~https://github.com/bucharest-gold/s2i-nodejs.git
+  sleep 59
+  oc expose svc/s2i-nodejs
+  sleep 20
+  curl $(oc get routes | awk '{print $2}' | grep xip.io)
+  printf "\n"
+  echo "================================================================"
+  echo "s2i-nodejs deployed and running with centos-s2i-nodejs:${TAG}."
+  echo "================================================================"
+done
+
+
+
