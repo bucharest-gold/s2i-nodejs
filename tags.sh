5be672e993294ab0c3ca3fc96dcfbadc7bf4b403
Helio Frota <00hf11@gmail.com>
2017-02-28 11:27:54 -0300

    build: waiting for oc command

--- tags.sh
+++ tags.sh
@@ -3,15 +3,34 @@
 oc cluster up
 oc login -u system:admin
 
+function waitForOc {
+  C=50
+  while [ $C -gt 0 ]
+  do
+    grep "Push successful" oc.log
+    if [ $? -eq 0 ]; then
+      echo "App deployed."
+      echo "" > oc.log
+      C=0
+    else
+      echo -n "."
+      C=$(( $C - 1 ))
+    fi
+    sleep 1
+  done
+}
+
 for TAG in $(wget -q https://registry.hub.docker.com/v1/repositories/bucharestgold/centos7-s2i-nodejs/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}');
 do
   TAGCHANGED=${TAG//./}
   TAGCHANGED=$(echo $TAGCHANGED | awk '{print tolower($0)}')
   oc new-project "project${TAGCHANGED}"
   oc new-app bucharestgold/centos7-s2i-nodejs:${TAG}~https://github.com/bucharest-gold/s2i-nodejs.git
-  sleep 150
+  sleep 30
+  oc logs -f bc/s2i-nodejs > oc.log 2>&1 &
+  waitForOc
   oc expose svc/s2i-nodejs
-  sleep 60
+  sleep 10
   echo "================================================================"
   curl $(oc get routes | awk '{print $2}' | grep xip.io)
   printf "\n"
