f44f428101cfa0c8fd521e6da10d4239defeeb48
Helio Frota <00hf11@gmail.com>
2017-02-27 22:59:22 -0300

    build: more time to oc new-app

--- tags.sh
+++ tags.sh
@@ -8,12 +8,12 @@ do
   TAGG=${TAG//./}
   oc new-project "project${TAGG}"
   oc new-app bucharestgold/centos7-s2i-nodejs:${TAG}~https://github.com/bucharest-gold/s2i-nodejs.git
-  sleep 60
+  sleep 120
   oc expose svc/s2i-nodejs
-  sleep 30
+  sleep 20
+  echo "================================================================"
   curl $(oc get routes | awk '{print $2}' | grep xip.io)
   printf "\n"
-  echo "================================================================"
   echo "s2i-nodejs deployed and running with centos-s2i-nodejs:${TAG}."
   echo "================================================================"
 done
