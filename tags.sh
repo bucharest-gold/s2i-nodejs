e8901de9d0d38de7be45b671c85fd831d205b800
Helio Frota <00hf11@gmail.com>
2017-02-27 23:41:25 -0300

    build: improvements * more sleep time added * travis cleanup

--- tags.sh
+++ tags.sh
@@ -5,12 +5,13 @@ oc login -u system:admin
 
 for TAG in $(wget -q https://registry.hub.docker.com/v1/repositories/bucharestgold/centos7-s2i-nodejs/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}');
 do
-  TAGG=${TAG//./}
-  oc new-project "project${TAGG}"
+  TAGCHANGED=${TAG//./}
+  TAGCHANGED=$(echo $TAGCHANGED | awk '{print tolower($0)}')
+  oc new-project "project${TAGCHANGED}"
   oc new-app bucharestgold/centos7-s2i-nodejs:${TAG}~https://github.com/bucharest-gold/s2i-nodejs.git
-  sleep 120
+  sleep 150
   oc expose svc/s2i-nodejs
-  sleep 20
+  sleep 60
   echo "================================================================"
   curl $(oc get routes | awk '{print $2}' | grep xip.io)
   printf "\n"
