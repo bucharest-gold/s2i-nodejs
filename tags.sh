b9415c0ea322a120eb523b9c51d629293a10d15b
Helio Frota <00hf11@gmail.com>
2017-03-01 16:52:16 -0300

    build: new strategy

--- tags.sh
+++ tags.sh
@@ -3,40 +3,32 @@
 oc cluster up
 oc login -u system:admin
 
-function waitForOc {
-  C=50
-  while [ $C -gt 0 ]
-  do
-    grep "Push successful" oc.log
-    if [ $? -eq 0 ]; then
-      echo "App deployed."
-      echo "" > oc.log
-      C=0
-    else
-      echo -n "."
-      C=$(( $C - 1 ))
-    fi
-    sleep 1
-  done
-}
-
 for TAG in $(wget -q https://registry.hub.docker.com/v1/repositories/bucharestgold/centos7-s2i-nodejs/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}');
 do
   TAGCHANGED=${TAG//./}
   TAGCHANGED=$(echo $TAGCHANGED | awk '{print tolower($0)}')
+
+  echo "Creating a Dockerfile with the tag: ${TAG}..."
+  echo "FROM bucharestgold/centos7-nodejs:${TAG}" > Dockerfile
+
+  echo "Creating new project with the tag: ${TAG}..."
   oc new-project "project${TAGCHANGED}"
-  oc new-app bucharestgold/centos7-s2i-nodejs:${TAG}~https://github.com/bucharest-gold/s2i-nodejs.git
-  sleep 30
-  oc logs -f bc/s2i-nodejs > oc.log 2>&1 &
-  waitForOc
-  oc expose svc/s2i-nodejs
-  sleep 10
+
+  echo "Creating ${TAG} s2inodejs build..."
+  oc new-build --binary --name=s2inodejs -l app=s2inodejs
+
+  echo "Building s2inodejs app..."
+  npm install
+  oc start-build s2inodejs --from-dir=. --follow
+
+  echo "Starting s2inodejs app..."
+  oc new-app s2inodejs -l app=s2inodejs
+  oc expose svc/s2inodejs
+
   echo "================================================================"
   curl $(oc get routes | awk '{print $2}' | grep xip.io)
   printf "\n"
-  echo "s2i-nodejs deployed and running with centos-s2i-nodejs:${TAG}."
+  echo "App deployed and running with centos-s2i-nodejs:${TAG}."
   echo "================================================================"
 done
-
-
 
